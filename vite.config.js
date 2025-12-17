c379b6a348145088a63d78eb4d6bc315ce315e0d
Harris-PS <psharris2002@gmail.com>
2025-12-17 15:02:00 +0530

    Added base to vite.config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/Counter-and-Feedback-Tracker/",
   plugins: [
     react({
       babel: {
