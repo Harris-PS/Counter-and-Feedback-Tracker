bab23b1c63bf41ea5e5de1317019782bade46045
Harris-PS <psharris2002@gmail.com>
2025-12-17 11:58:44 +0530

    Created the counter component

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Counter from "./Counter.jsx"
+import Feedback from "./Feedback.jsx"
+
+
+function App() {
+
+  return (
+    <>
+    <Counter/>
+    <Feedback/>
+    </>
+    
+  )
+}
+
+export default App
---
+++ src/Feedback.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react"
+
+const [text, setText] = useState("");
+function Feedback(){
+  return(
+    <form action="text"></form>
+    
+  )
+
+}
+
+export default Feedback
